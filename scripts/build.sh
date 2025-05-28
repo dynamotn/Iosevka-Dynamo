fa1822f9f3d52a239eb5dbab861bb4c879e28fe6
Trần Đức Nam <me@dynamotn.dev>
2025-05-28 19:57:18 +0700

    refactor: rename nerd font

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
-# shellcheck disable=1091
-. "$SCRIPT_DIR/lib/dybatpho/init"
+# shellcheck source=lib/dybatpho/init.sh
+. "$SCRIPT_DIR/lib/dybatpho/init.sh"
 dybatpho::register_err_handler
 
 FONT_FAMILY_NAME="Iosevka Dynamo"
@@ -10,9 +10,9 @@ FIRACODE_VERSION="3.1"
 IOSEVKA_VERSION="latest"
 IOSEVKA_VARIANT="FixedSS05" # Use fixed version of Fira Mono style variant
 
-ASSETS_DIR="$SCRIPT_DIR/../assets"
-OUTPUT_DIR="$SCRIPT_DIR/../build"
-SRC_DIR="$SCRIPT_DIR/../src"
+ASSETS_DIR=$(realpath "$SCRIPT_DIR/../assets")
+OUTPUT_DIR=$(realpath "$SCRIPT_DIR/../build")
+SRC_DIR=$(realpath "$SCRIPT_DIR/../src")
 
 prerequisite() {
   dybatpho::require 'grep'
@@ -22,7 +22,11 @@ prerequisite() {
 }
 
 download_iosevka() {
-  if [ "$IOSEVKA_VERSION" == "latest" ]; then
+  if dybatpho::is file "$ASSETS_DIR/IosevkaFixedSS05-Regular.ttf"; then
+    dybatpho::info "Already have Iosevka font"
+    return
+  fi
+  if [ "${IOSEVKA_VERSION}" == "latest" ]; then
     local temp_file_1=$(mktemp)
     dybatpho::cleanup_file_on_exit "$temp_file_1"
     dybatpho::curl_do https://api.github.com/repos/be5invis/Iosevka/releases/latest "$temp_file_1"
@@ -34,7 +38,7 @@ download_iosevka() {
   dybatpho::curl_download \
     "https://github.com/be5invis/Iosevka/releases/download/${IOSEVKA_VERSION}/PkgTTF-Iosevka${IOSEVKA_VARIANT}-${IOSEVKA_VERSION:1}.zip" \
     "$temp_file_2"
-  unzip -qqo "$temp_file_2" -d "$ASSETS_DIR"
+  unzip -qqo "$temp_file_2" -d "${ASSETS_DIR}"
   dybatpho::success "Downloaded Iosevka"
 }
 
@@ -42,6 +46,10 @@ download_firacode() {
   dybatpho::notice "Downloading FiraCode font version: ${FIRACODE_VERSION}"
   local fira_style_names=("Regular" "Bold")
   for style in "${fira_style_names[@]}"; do
+    if dybatpho::is file "${ASSETS_DIR}/FiraCode-${style}.otf"; then
+      dybatpho::info "Already have FiraCode ${style}"
+      continue
+    fi
     dybatpho::curl_download \
       "https://raw.githubusercontent.com/tonsky/FiraCode/${FIRACODE_VERSION}/distr/otf/FiraCode-${style}.otf" \
       "${ASSETS_DIR}/FiraCode-${style}.otf"
@@ -56,8 +64,9 @@ patch_nerd() {
     -v "$OUTPUT_DIR/${input_filename}":/in/iosevka-dynamo.ttf \
     -v "$OUTPUT_DIR":/out \
     nerdfonts/patcher \
+    --name "'Iosevka Dynamo Nerd'" \
     --mono --fontawesome --codicons --material --octicons --careful
-  local patched_filename="IosevkaDynamoNerdFontMonoPlusFontAwesomePlusOcticonsPlusCodiconsPlusMaterialDesignIcons-Regular.ttf"
+  local patched_filename="IosevkaDynamoNerd-Regular.ttf"
   local output_filename="iosevka-dynamo-nerd.ttf"
   sudo mv "${OUTPUT_DIR}/${patched_filename}" \
     "${OUTPUT_DIR}/${output_filename}"
